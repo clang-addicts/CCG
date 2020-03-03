5f5fdbb78c2e91553a5315e52a7349fa49c91063
Ash <ashkim2001@gmail.com>
2020-03-03 22:33:11 +0900

    study code upload
    input related study code uploaded

--- src/main.c
+++ src/main.c
@@ -8,4 +8,26 @@
 int main(int argc, char **argv)
 {
 	display();	
+	/*
+	while(1)
+	{
+		if(kbhit())
+		{
+			in=getch();
+			switch(in)
+			{
+				case 72: update(1); break;
+				case 80: update(2); break;
+				case 13:{
+					switch(MENU_Y){
+						case 12: weapon();
+						case 15: help();
+						case 18: exit(0);
+					}
+				}
+			}
+		}
+	}
+	*/
+	
 }
