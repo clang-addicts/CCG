171451ccde556d0b231222baf9585400f8c86b16
ashzHax <ashkim2001@gmail.com>
2020-03-04 12:09:32 +0900

    study hint upload

--- src/main.c
+++ src/main.c
@@ -5,29 +5,21 @@
 
 #include "display.c"
 
+#define ASCII_KEY_ESC 27
+
 int main(int argc, char **argv)
 {
 	display();	
-	/*
+	int in;
 	while(1)
 	{
 		if(kbhit())
 		{
-			in=getch();
-			switch(in)
-			{
-				case 72: update(1); break;
-				case 80: update(2); break;
-				case 13:{
-					switch(MENU_Y){
-						case 12: weapon();
-						case 15: help();
-						case 18: exit(0);
-					}
-				}
+			in = getch();				
+			printf("%d\n",in);
+			if( in == ASCII_KEY_ESC ) {
+				return 0;
 			}
 		}
 	}
-	*/
-	
 }
