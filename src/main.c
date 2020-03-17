035bc4a05fc2b7643fcbb0e9e1706faf8aa8a2fe
gggyjw <57584910+gggyjw@users.noreply.github.com>
2020-03-17 13:25:19 +0900

    move

--- src/main.c
+++ src/main.c
@@ -32,74 +32,74 @@ int main(int argc, char **argv)
 	int user_input = 0;
 	int user_selection = 0;
 
-	init();
-	display_menu();
+//	init();
+//	display_menu();
 	
-	while(1)
-	{
-		if( kbhit() )
-		{
-			user_input = getch();
-			switch( user_input )
-			{
-				case ASCII_ESC: 
-				{
-					quit_program();
-				}
-				
-				case ASCII_ENTER:
-				{
-					switch( user_selection )
-					{
-						case 0:
-						{
-							// START GAME
-							//init_snake();
-							break;
-						}
-						case 1:
-						{
-							// END GAME
-							quit_program();
-							break;
-						}
-					}
-				} 
-				// SELECTIONS
-				case 'w':
-				case 'W':
-				//case ASCII_ARROW_UP:
-				{
-					user_selection = 0;
-					display_select_up();
-					break;
-				}
-				/*
-				case ASCII_A:
-				case ASCII_a:
-				case ASCII_ARROW_LEFT:
-				{
-					display_select_left();
-					break;
-				} */				
-				case 's':
-				case 'S':
-			//	case ASCII_ARROW_DOWN:
-				{
-					user_selection = 1;
-				    display_select_down();
-					break;
-				} 
-		/*		case ASCII_D:
-				case ASCII_d:
-				case ASCII_ARROW_RIGHT:
-				{
-					display_select_right();
-					break;
-				}	*/	
-			}
-		}
-	}
+//	while(1)
+//	{
+//		if( kbhit() )
+//		{
+//			user_input = getch();
+//			switch( user_input )
+//			{
+//				case ASCII_ESC: 
+//				{
+//					quit_program();
+//				}
+//				
+//				case ASCII_ENTER:
+//				{
+//					switch( user_selection )
+//					{
+//						case 0:
+//						{
+//							// START GAME
+//							//init_snake();
+//							break;
+//						}
+//						case 1:
+//						{
+//							// END GAME
+//							quit_program();
+//							break;
+//						}
+//					}
+//				} 
+//				// SELECTIONS
+//				case 'w':
+//				case 'W':
+//				//case ASCII_ARROW_UP:
+//				{
+//					user_selection = 0;
+//					display_select_up();
+//					break;
+//				}
+//				/*
+//				case ASCII_A:
+//				case ASCII_a:
+//				case ASCII_ARROW_LEFT:
+//				{
+//					display_select_left();
+//					break;
+//				} */				
+//				case 's':
+//				case 'S':
+//			//	case ASCII_ARROW_DOWN:
+//				{
+//					user_selection = 1;
+//				    display_select_down();
+//					break;
+//				} 
+//		/*		case ASCII_D:
+//				case ASCII_d:
+//				case ASCII_ARROW_RIGHT:
+//				{
+//					display_select_right();
+//					break;
+//				}	*/	
+//			}
+//		}
+//	}
 
 	move();
 
--- src/move.c
+++ src/move.c
@@ -8,7 +8,7 @@ int move(){
 	char in;
 	int screen[x][y];
 	
-	for(i = 1; i <= 50; i++){
+	for(i = 1; i  <= 50; i++){
 		for(j = 1; j <= 50; j++){
 			screen[j][i] = printf("  ");
 			SetColor(230);
@@ -21,18 +21,26 @@ int move(){
 	int x = 0, y = 0;
 	while(1){
 		gotoxy(x, y);
-		printf(" ");
+		printf("  ");
 		if(kbhit()){
 			in=getch();
 			switch(in){
-				case 'w' : y--;  break;
-				case 'a' : x--;	 break;
-				case 's' : y++;	 break;
-				case 'd' : x++;	 break;
+				case 'w' : y--; break;
+				case 'a' : x--;	break;
+				case 's' : y++;	break;
+				case 'd' : x++;	break;
+				case 'W' : y--; break;
+				case 'A' : x--; break;
+				case 'S' : y++; break;
+				case 'D' : x++; break;
+				case 72 : y--; break;
+				case 75 : x--; break;
+				case 80 : y++; break;
+				case 77 : x++; break;
 			}	
 
 		}
 		gotoxy(x, y);
-		printf("*");
+		printf("**");
 	}
 }
