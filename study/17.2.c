5654e7ba1439dd341bb82d70c6a53c1288cd2619
gggyjw <57584910+gggyjw@users.noreply.github.com>
2020-04-24 13:31:59 +0900

    ahhhhhhhhhh

---
+++ study/17.2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+	int i, j;
+	int a[5][5] = {
+	{1, 2, 3, 4, 5},
+	{6, 7, 8, 9, 10},
+	{11, 12, 13, 14, 15},
+	{16, 17, 18, 19, 20},
+	{21, 22, 23, 24, 25}
+    };
+	
+	printf("%d ", a[0][0]);
+	printf("%d ", a[0][1]);
+	printf("%d ", a[0][2]);
+	printf("%d ", a[0][3]);
+	printf("%d \n", a[0][4]);
+	printf("%d ", a[1][0]);
+	printf("%d ", a[1][1]);
+	printf("%d ", a[1][2]);
+	printf("%d ", a[1][3]);
+	printf("%d \n", a[1][4]);
+	printf("%d ", a[2][0]);
+	printf("%d ", a[2][1]);
+	printf("%d ", a[2][2]);
+	printf("%d ", a[2][3]);
+	printf("%d \n", a[2][4]);
+	printf("%d ", a[3][0]);
+	printf("%d ", a[3][1]);
+	printf("%d ", a[3][2]);
+	printf("%d ", a[3][3]);
+	printf("%d \n", a[3][4]);
+	printf("%d ", a[4][0]);
+	printf("%d ", a[4][1]);
+	printf("%d ", a[4][2]);
+	printf("%d ", a[4][3]);
+	printf("%d ", a[4][4]);
+	
+}
