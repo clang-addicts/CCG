4de68e112dd79e04bfdf35a3434ee9e4a87090a9
gggyjw <57584910+gggyjw@users.noreply.github.com>
2020-06-24 16:23:59 +0900

    choose posituon for  square

---
+++ src/123aad.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <windows.h>
+
+#include "function.c"
+
+int main (){
+	int posX, posY;
+	
+	while(1){
+		SetColor(1);
+		gotoxy(0, 0);
+		printf("choose position : ");
+		gotoxy(18, 0);
+		scanf("%d %d", &posX, &posY);
+		gotoxy(posX, posY);
+		SetColor(200);
+		printf("  ");
+		gotoxy(18, 0);
+	}
+}
+
