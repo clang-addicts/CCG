12fb30a98d19f8c2f19aa11c2eb5c5fec96abf53
ashzHax <ashkim2001@gmail.com>
2020-06-25 13:58:33 +0900

    test code commit

--- function/gotoxy.h
+++ function/gotoxy.h
@@ -1,14 +1,22 @@
 #ifndef _GOTOXY_H_
 #define _GOTOXY_H_
 
-void SetColor(int color) 
-{
-	SetConsoleTextAttribute (GetStdHandle (STD_OUTPUT_HANDLE), color);
-}
+#define <windows.h> /* calling Mircosoft Console API */
 
-void gotoxy(short X, short Y)
-{	
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),(COORD){X,Y});
-}
+/*
+    CONSOLE_SET_COLOR( (int)0~255 )
+    return : 
+        0 = fail
+        !0 = success
+*/
+#define CONSOLE_SET_COLOR(clr) (SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),clr))
 
+/*
+    CONSOLE_SET_CPOS(X,Y)
+    return : 
+        0 = fail
+        !0 = success
+*/
+#define CONSOLE_SET_CPOS(X,Y) (SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),(COORD){X,Y}))
+	
 #endif	
